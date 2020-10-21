6aa3b43b29dfbcf94c5342384fb1816c14694c19
Vítor Cardoso Bertolucci <vitorbertolucci@gmail.com>
2020-10-20 21:25:59 -0300

    Implementa tela de cadastro

---
+++ src/pages/CreateOrphanage.tsx
@@ -0,0 +1,192 @@
+import React, { ChangeEvent, FormEvent, useState } from "react";
+import { Marker } from "react-leaflet";
+import { LeafletMouseEvent } from "leaflet";
+
+import PrimaryButton from "../components/PrimaryButton";
+import Sidebar from "../components/Sidebar";
+
+import { FiPlus } from "react-icons/fi";
+import Map from "../components/Map";
+import happyMapIcon from "../utils/mapIcon";
+
+import "../styles/pages/create-orphanage.css";
+import Orphanage from "./Orphanage";
+import api from "../services/api";
+import { useHistory } from "react-router-dom";
+
+export default function OrphanagesMap() {
+  const history = useHistory();
+  const [position, setPosition] = useState({ latitude: 0, longitude: 0 });
+  const [name, setName] = useState("");
+  const [about, setAbout] = useState("");
+  const [instructions, setInstructions] = useState("");
+  const [openingHours, setOpeningHours] = useState("");
+  const [openOnWeekends, setOpenOnWeekends] = useState(true);
+  const [images, setImages] = useState<File[]>([]);
+  const [previewImages, setPreviewImages] = useState<string[]>([]);
+
+  const handleMapClick = (event: LeafletMouseEvent) => {
+    const { lat, lng } = event.latlng;
+
+    setPosition({
+      latitude: lat,
+      longitude: lng
+    });
+  };
+
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+
+    const { latitude, longitude } = position;
+
+    const data = new FormData();
+    data.append("name", name);
+    data.append("about", about);
+    data.append("latitude", String(latitude));
+    data.append("longitude", String(longitude));
+    data.append("instructions", instructions);
+    data.append("opening_hours", openingHours);
+    data.append("open_on_weekends", String(openOnWeekends));
+
+    images.forEach(image => {
+      data.append("images", image);
+    });
+
+    await api.post("orphanages", data);
+
+    alert("Cadastro realizado com sucesso!");
+    history.push("/app");
+  };
+
+  const handleSelectImages = (event: ChangeEvent<HTMLInputElement>) => {
+    if (!event.target.files) {
+      return;
+    }
+
+    const selectedImages = Array.from(event.target.files);
+    setImages(selectedImages);
+
+    const selectedImagesPreview = selectedImages.map(image =>
+      URL.createObjectURL(image)
+    );
+    setPreviewImages(selectedImagesPreview);
+  };
+
+  return (
+    <div id="page-create-orphanage">
+      <Sidebar />
+
+      <main>
+        <form className="create-orphanage-form" onSubmit={handleSubmit}>
+          <fieldset>
+            <legend>Dados</legend>
+
+            <Map
+              style={{ width: "100%", height: 280 }}
+              onclick={handleMapClick}
+            >
+              {position.latitude !== 0 && (
+                <Marker
+                  interactive={false}
+                  icon={happyMapIcon}
+                  position={[position.latitude, position.longitude]}
+                />
+              )}
+            </Map>
+
+            <div className="input-block">
+              <label htmlFor="name">Nome</label>
+              <input
+                id="name"
+                value={name}
+                onChange={e => {
+                  setName(e.target.value);
+                }}
+              />
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="about">
+                Sobre <span>Máximo de 300 caracteres</span>
+              </label>
+              <textarea
+                id="about"
+                maxLength={300}
+                value={about}
+                onChange={e => setAbout(e.target.value)}
+              />
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="images">Fotos</label>
+
+              <div className="images-container">
+                {previewImages.map(image => {
+                  return <img key={image} src={image} alt={name} />;
+                })}
+
+                <label htmlFor="image[]" className="new-image">
+                  <FiPlus size={24} color="#15b6d6" />
+                </label>
+              </div>
+
+              <input
+                type="file"
+                id="image[]"
+                multiple
+                onChange={handleSelectImages}
+              />
+            </div>
+          </fieldset>
+
+          <fieldset>
+            <legend>Visitação</legend>
+
+            <div className="input-block">
+              <label htmlFor="instructions">Instruções</label>
+              <textarea
+                id="instructions"
+                value={instructions}
+                onChange={e => setInstructions(e.target.value)}
+              />
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="opening_hours">Nome</label>
+              <input
+                id="opening_hours"
+                value={openingHours}
+                onChange={e => setOpeningHours(e.target.value)}
+              />
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="open_on_weekends">Atende fim de semana</label>
+
+              <div className="button-select">
+                <button
+                  type="button"
+                  className={openOnWeekends ? "active" : ""}
+                  onClick={() => setOpenOnWeekends(true)}
+                >
+                  Sim
+                </button>
+                <button
+                  type="button"
+                  className={!openOnWeekends ? "active" : ""}
+                  onClick={() => setOpenOnWeekends(false)}
+                >
+                  Não
+                </button>
+              </div>
+            </div>
+          </fieldset>
+
+          <PrimaryButton type="submit">Confirmar</PrimaryButton>
+        </form>
+      </main>
+    </div>
+  );
+}
+
+// return `https://a.tile.openstreetmap.org/${z}/${x}/${y}.png`;
